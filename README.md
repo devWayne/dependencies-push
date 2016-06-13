b4c473a2d104ffcebb0f17bea953bae7007b782f
珈文 <jiawen.czq@alibaba-inc.com>
2016-06-13 17:24:49 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# dependencies-push
+## 安装方式：
+
+```
+$sudo tnpm i -g dppush
+```
+
+
+## 使用方式
+
+- 新建一个目录，用于存放升级过程中的仓库，配置和日志文件
+- 编写配置文件，并保存为`pushConfig.json`
+
+  - 配置文件实例如下：
+  
+```
+{
+    "gitList": ["chaoshi-history", 
+    {
+        "name": "chaoshi-liangfan",
+        "version": "3.0.1"
+    }],
+    "muiInfo": {
+        "name": "mui/chaoshi-base",
+        "version": "4.2.34"
+    },
+    "svnBranch": "http://svn.app.taobao.net/repos/tmallwap/branches/20160606_695855_gulptest_1/"
+}
+```
+
+- 执行`dppush`,在执行`tnpm i`等待时间较长，可能会误以为进程停止执行
+- 验证方式：
+  - 直接打开配置文件中的`svnBranch`，找到对应的seed文件进行验证
+  - 打开当前目录下的`stdout.log`查看结构结果
+  
+## 配置文件详解
+
+- `gitList`里存放`业务仓库`
+  - 对象类型可以是2种，如果是`String`类型的，默认升级一个小版本，如`4.0.3=>4.0.4`.
+  - 如果是`Object`类型，必须包含`name`和`version`
+- `muiInfo`里存放`需要同步版本的组件`,必须包含`name`和`version`
+
+
+## TodoList
+- `muiInfo`支持数组，同时可升级多个组件
+- 纵向依赖分析，组件升级
--- workflow.js
+++ workflow.js
@@ -1,58 +1,64 @@
+'use strict';
+
+
 const fs = require('fs'),
     _exec = require('./lib/cli'),
     format = require('./lib/format'),
-    version = require('./lib/version');
+    version = require('./lib/version'),
+    series = require('./lib/series');
 
 
 module.exports = {
-    /*
-        updateSvnLoad: (chaoshi, svnBranch) => {
-            _exec('cd test && git clone git@gitlab.alibaba-inc.com:tm/' + chaoshi + '.git', 'clone ' + chaoshi + '仓库')
-                .then((res) => {
-                    return res;
-                    // _exec('cd test/chaoshi-history','安装tnpm');
-                })
-                .then((res) => {
-                    console.log(__dirname);
-                    const data = fs.readFileSync("test/" + chaoshi + "/package.json", 'utf-8');
-                    const pkg = JSON.parse(data);
-                    var _version = version.updateVersion(pkg.version);
-                    pkg.svnBranch = svnBranch;
-                    fs.writeFile("test/" + chaoshi + "/package.json", format(JSON.stringify(pkg)));
-                    return pkg;
-                })
-                .then((res) => {
-                    _exec('cd test/' + chaoshi + ' && gulp svnload', '执行构建操作');
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
+
+    updateSvnload: function(chaoshi, muiInfo, svnBranch) {
+        var chaoshiVersion, chaoshiName, chaoshiInfo;
+        if (typeof chaoshi == 'string') {
+            chaoshiName = chaoshi;
+        } else {
+            chaoshiInfo = chaoshi;
+            chaoshiName = chaoshiInfo.name;
         }
-    }*/
-}
+        return _exec('git clone git@gitlab.alibaba-inc.com:tm/' + chaoshiName + '.git', 'clone ' + chaoshiName + '仓库')
+            .then((res) => {
+                return _exec('cd ' + chaoshiName + ' && tnpm i', '安装tnpm');
+            })
+            .then((res) => {
+                const data = fs.readFileSync(chaoshiName + "/package.json", 'utf-8');
+                const pkg = JSON.parse(data);
+                if (typeof chaoshi == 'string') {
+                    chaoshiVersion = pkg.version = version.updateVersion(pkg.version);
+                } else {
+                    chaoshiVersion = pkg.version = chaoshiInfo.version;
+                }
+                pkg.svnBranch = svnBranch;
+                pkg.feDependencies[muiInfo.name] = muiInfo.version;
+                fs.writeFile(chaoshiName + "/package.json", format(JSON.stringify(pkg)));
+                return pkg;
+            })
+            .then((res) => {
+                return _exec('cd ' + chaoshiName + ' && git checkout -b daily/' + chaoshiVersion, '新建分支');
+            })
+            .then((res) => {
+                return _exec('cd ' + chaoshiName + ' && gulp svnload', '执行构建操作');
+            })
+            .then((res) => {
+                return _exec('cd ' + chaoshiName + ' && git add --a && git commit -m updateVersion', '提交git commit');
+            })
+            .then((res) => {
+                return _exec('cd ' + chaoshiName + ' && git push origin daily/' + chaoshiVersion, '提交git commit');
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    },
 
-function updateSvnLoad(chaoshi, svnBranch) {
-    _exec('cd test && git clone git@gitlab.alibaba-inc.com:tm/' + chaoshi + '.git', 'clone ' + chaoshi + '仓库')
-        .then((res) => {
-           return _exec('cd test/'+chaoshi+' && tnpm i','安装tnpm');
-        })
-        .then((res) => {
-            console.log(__dirname);
-            const data = fs.readFileSync("test/" + chaoshi + "/package.json", 'utf-8');
-            const pkg = JSON.parse(data);
-            var _version = version.updateVersion(pkg.version);
-            pkg.svnBranch = svnBranch;
-            fs.writeFile("test/" + chaoshi + "/package.json", format(JSON.stringify(pkg)));
-            return pkg;
-        })
-        .then((res) => {
-            _exec('cd test/' + chaoshi + ' && gulp svnload', '执行构建操作');
-        })
-        .catch((e) => {
-            console.log(e);
+    updateAllSvn: function(gitList, muiInfo, svnBranch) {
+        const _pList = gitList.map((chaoshi, index) => {
+            return this.updateSvnload(chaoshi, muiInfo, svnBranch)
         });
+        series(_pList).then((res) => {
+            console.log('－－－－－－－－－流程结束－－－－－－－－');
+        });
+    }
+
 }
-
-
-
-updateSvnLoad('chaoshi-history', 'http://svn.app.taobao.net/repos/tmallwap/branches/20160606_695855_gulptest_1/');
